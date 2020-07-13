958c14846651e58ecaaabd3f5399dca3689f5fdb
Mirica Yancey <myancey@fnal.gov>
2020-07-13 15:39:47 -0500

    Big Update
    Updated unit tests, seperated condor + glidein scriipts, optimized speed, created test directories

--- regex_test.py
+++ regex_test.py
@@ -1,25 +1,14 @@
-import re
-import mmap
-import argparse
-import os
+import re, mmap, argparse, os, logging
 
-#Replace Terms
 IPV4 = "XX.XX.XX.XX"
 IPV6 ="XXXX:XXXX:XXXX:XXXX::"
 USER = "USER"
+CONDORS = ["MasterLog", "StartdLog", "StarterLog", "StartdHistory"]
 IP_REGEX = ["(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)", "(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|^::(?:[0-9a-fA-F]{1,4}:){0,6}[0-9a-fA-F]{1,4}$|^[0-9a-fA-F]{1,4}::(?:[0-9a-fA-F]{1,4}:){0,5}[0-9a-fA-F]{1,4}$|^[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}::(?:[0-9a-fA-F]{1,4}:){0,4}[0-9a-fA-F]{1,4}$|^(?:[0-9a-fA-F]{1,4}:){0,2}[0-9a-fA-F]{1,4}::(?:[0-9a-fA-F]{1,4}:){0,3}[0-9a-fA-F]{1,4}$|^(?:[0-9a-fA-F]{1,4}:){0,3}[0-9a-fA-F]{1,4}::(?:[0-9a-fA-F]{1,4}:){0,2}[0-9a-fA-F]{1,4}$|^(?:[0-9a-fA-F]{1,4}:){0,4}[0-9a-fA-F]{1,4}::(?:[0-9a-fA-F]{1,4}:)?[0-9a-fA-F]{1,4}$|^(?:[0-9a-fA-F]{1,4}:){0,5}[0-9a-fA-F]{1,4}::[0-9a-fA-F]{1,4}$|(?:[0-9a-fA-F]{1,4}:){0,6}[0-9a-fA-F]{1,4}::"]
 
-    
-def findEmail(filename): #CONDOR SPECIFIC finds and returns user email (before @symbol)
-    lis = ''
-    with open(filename, 'rb', 0) as file, \
-        mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
-        if s.find(b'x509UserProxyEmail') != -1:
-            x = s.find(b'x509UserProxyEmail')
-            end = s.find(b'@',x)
-            lis = (((s[x: end].split(b'='))[1].decode("utf-8")).replace('"', '')).replace(' ', '')   
-    return lis
-            
+
+
+#Would like to find a way to combine findglideinuser and findcondoruser
 def findCondorUserIDs(filename): #finds and returns the CN user identifiers
     lis = []
     with open(filename, 'rb', 0) as file, \
@@ -36,107 +25,64 @@ def findCondorUserIDs(filename): #finds and returns the CN user identifiers
             lis = lis_new
     return lis
 
-#regex version of condor remove data
-#Negatives: reads line by line. Bad if the process is stopped halfway through
-def findGlideinUserIDs(filename): #Greedy CN Replacer
+def findGlideinUserIDs(filename):
     cns = []
-    f_in = open(filename, "r")
-    outlines = f_in.readlines()
-    f_in.close()
-    cn_regex = 'CN.*(")'
-    for line in outlines: #using search instead of find all means there cannot be more than one instance in a line
-      if re.search(cn_regex, line, flags=re.MULTILINE):
-        idx = re.search('CN', line).start()
-        end = line.find('"',idx)
-        line = line[idx:end]
-        cns.append(line)
-        
-    #might become a long process with longer files
+    with open(filename,'r', encoding="utf8") as f_in:
+        line = f_in.read()
+        cn_regex = 'CN.*(")'
+        if re.search(cn_regex, line, flags=re.MULTILINE):
+            idx = re.search('CN', line).start()
+            end = line.find('"',idx)
+            line = line[idx:end]
+            cns.append(line)
     cns = ("".join(cns)).split("CN=")
     cns = ("".join(cns)).split("CN\\=")
-    cns = (" ".join(cns)).split(" ")    
+    cns = (" ".join(cns)).split(" ")
     return cns
-  
-def cleanCondorInfo(filename,email,userinfo): #removes email 6 user data (name, email) from a file
-    f = open(filename,'r')
-    filedata = f.read()
-    f.close()
-    newdata = filedata.replace(email, USER)
-    for x in range(0, len(userinfo)):
-        newdata = newdata.replace(userinfo[x], USER)
-    return newdata
 
-def cleanGlideinUser(filename, recog):
-    f = open(filename,'r')
-    filedata = f.read()
-    f.close()
-    newdata = filedata.replace(recog[0], USER)
-    for x in range(1, len(recog)):
-        newdata = newdata.replace(recog[x], USER)
-        newdata = newdata.replace(recog[x], USER)
-        
-    return newdata
-    
+def findEmail(filename): #CONDOR SPECIFIC finds and returns user email (before @symbol)
+    lis = ''
+    with open(filename, 'rb', 0) as file, \
+        mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
+        if s.find(b'x509UserProxyEmail') != -1:
+            x = s.find(b'x509UserProxyEmail')
+            end = s.find(b'@',x)
+            lis = (((s[x: end].split(b'='))[1].decode("utf-8")).replace('"', '')).replace(' ', '')   
+    return lis
+            
+def cleanCondorUser(filename,email,userinfo): #removes email & user data (name, email) from a file
+   with open(filename,'r', encoding="utf8") as f:
+        filedata = f.read()
+        newdata = filedata.replace(email, USER)
+        for x in range(0, len(userinfo)):
+            newdata = newdata.replace(userinfo[x], USER)
+        return newdata
+
+def cleanGlideinUser(filename, recog): #removes all instance of user
+   with open(filename,'r', encoding="utf8") as f:
+        filedata = f.read()
+        newdata = filedata.replace(recog[0], USER)
+        for x in range(1, len(recog)):
+            newdata = newdata.replace(recog[x], USER)
+            newdata = newdata.replace(recog[x], USER)
+        newdata = re.sub(rf"{recog[0]}[\w \W]{recog[1]}", USER ,newdata, flags=re.I + re.M)
+        return newdata
 
 def replaceIP(filename):
     newdata = ""
-    f_in = open(filename,"r")
-    outlines = f_in.readlines()
-    for line in outlines: #super specific list indexes for now
-        if re.search(IP_REGEX[0], line, flags=re.MULTILINE) or re.search(IP_REGEX[1], line, flags=re.MULTILINE):
-            if re.findall(IP_REGEX[0], line, flags=re.MULTILINE):
-                newdata = newdata + re.sub(IP_REGEX[0], IPV4, line, flags=re.MULTILINE)
-            elif re.findall(IP_REGEX[1], line, flags=re.MULTILINE):
-                newdata = newdata + re.sub(IP_REGEX[1], IPV6, line, flags=re.MULTILINE)
-            else:
-                newdata = newdata + line
-        else:
-            newdata = newdata + line
-    f_in.close()
-    return newdata
+    with open(filename,"r", encoding="utf8") as f_in:
+        outlines = f_in.read()
+        newdata = re.sub(IP_REGEX[0], IPV4, outlines)
+        newdata = re.sub(IP_REGEX[1], IPV6, newdata)
+        return newdata
 
 def overwrite(filename, data):
-    f_out = open(filename, "w")
-    f_out.seek(0)
-    f_out.write(data)
-    f_out.truncate()
-    f_out.close()    
-
-
-def cleanLogs():
-    # Parse command line arguments
-    parser = argparse.ArgumentParser(description="GlideinMonitor's Filtering")
-    parser.add_argument('-i', help="Input Directory", required=True)
-    parser.add_argument('-o', help="Output Directory", required=True)
-    args = parser.parse_args()
-    input_directory = args.i
-    output_directory = args.o
-
-    # In practice, use a loop until no files have been found in the input directory for better performance
-    input_directory_files = [file for file in os.listdir(input_directory) if os.path.isfile(os.path.join(input_directory, file))]
-
-    for file_name in input_directory_files:     # Iterate through each file in the input directory
-        with open(os.path.join(input_directory, file_name), 'r') as input_file_handle:         # Read in the file from the input directory
-            input_file_contents = input_file_handle.read()
-
-        if os.path.splitext(file_name)[1] == '.out' or os.path.splitext(file_name)[1] == '.err' : #may need overwrite method here
-            ids = findGlideinUserIDs(input_file_handle)
-            output_file_contents = cleanGlideinUser(input_file_handle, ids)
-            output_file_contents = replaceIP(input_file_handle)
-        else: #may need overwrite method here
-            ids = findCondorUserIDs(input_file_handle)
-            email = findEmail(input_file_handle)
-            output_file_contents = cleanCondorInfo(input_file_handle, email, ids)
-
-        # Write the file to the output directory
-        with open(os.path.join(output_directory, file_name), 'w') as file:
-            file.write(output_file_contents)
-
-        os.remove(os.path.join(input_directory, file_name))  # Delete the file from the input directory
-        
+    with open(filename,'w', encoding="utf8") as f_out:
+        f_out.seek(0)
+        f_out.write(data)
+        f_out.truncate()
 
 def cleanGlidein(file1):
-    #Glidein Logs
     user_ids = findGlideinUserIDs(file1)
     data = cleanGlideinUser(file1, user_ids)
     overwrite(file1, data)
@@ -146,19 +92,79 @@ def cleanGlidein(file1):
 def cleanCondor(file2):
     user_ids2 = findCondorUserIDs(file2)
     email = findEmail(file2)
-    co_data = cleanCondorInfo(file2,email,user_ids2)
+    co_data = cleanCondorUser(file2,email,user_ids2)
     overwrite(file2, co_data)
-    
-if __name__ == "__main__":
-    
-    #cleanLogs()
+    clean_data = replaceIP(file2)
+    overwrite(file2, clean_data)
 
-    #Condor Logs
-    """ file2 = "condor_logs/job.1.StartdLog.txt"
-    user_ids2 = findCondorUserIDs(file2)
-    email = findEmail(file2)
-    co_data = cleanCondorInfo(file2,email,user_ids2)
-    overwrite(file2, co_data) """
+def condorFile(file_name):
+    for i in range(0, len(CONDORS)):
+        if CONDORS[i] in file_name:
+            return True
+    return False
+        
+def cleanLogs():
+    # parser = argparse.ArgumentParser(description="GlideinMonitor's Filtering")     # Parse command line arguments
+    # parser.add_argument('-i', help="Input Directory", required=True)
+    # parser.add_argument('-o', help="Output Directory", required=True)
+    # args = parser.parse_args()
+    # input_directory = args.i
+    # output_directory = args.o
+
+    # In practice, use a loop until no files have been found in the input directory for better performance
+    # input_directory_files = [
+    #     file 
+    #     for file in os.listdir(input_directory) 
+      
+    #     if os.path.isfile(
+    #         os.path.join(input_directory, file)
+    #         )
+    #     ]
     
-    #cleanGlidein("regular_logs/overall_test_in.txt")
+    input_directory_files = []
+    input_directory = "input_dir"
+    output_directory = "output_dir"
     
+    while True:
+        for file in os.listdir(input_directory):
+            if os.path.isfile(os.path.join(input_directory, file)):
+                input_directory_files.append(file)
+            else:
+                False
+
+    for file_name in input_directory_files:  
+        #with open(os.path.join(input_directory, file_name), 'r') as input_file_handle:
+            #input_file_contents = input_file_handle.read()
+        if os.path.splitext(file_name)[1] == '.out' or os.path.splitext(file_name)[1] == '.err' : 
+            cleanGlidein(file_name)
+        elif condorFile(file_name) == True: 
+            cleanCondor(file_name)
+
+        with open(os.path.join(output_directory, file_name), 'w') as file:
+            output_file_contents = file_name.read()
+            file.write(output_file_contents)
+
+        os.remove(os.path.join(input_directory, file_name)) 
+
+if __name__ == "__main__":
+    cleanGlidein("regular_logs/overall_test_in.txt")
+    logger = logging.getLogger(__name__)
+    # Create handlers
+    c_handler = logging.StreamHandler()
+    f_handler = logging.FileHandler('file.log')
+    c_handler.setLevel(logging.WARNING)
+    f_handler.setLevel(logging.ERROR)
+
+    # Create formatters and add it to handlers
+    c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+    f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    c_handler.setFormatter(c_format)
+    f_handler.setFormatter(f_format)
+
+    # Add handlers to the logger
+    logger.addHandler(c_handler)
+    logger.addHandler(f_handler)
+    logging.info(cleanGlidein("regular_logs/overall_test_in.txt"))
+    
+    #-h option to print option of how to work this page
+    #documentation
--- test_filter.py
+++ test_filter.py
@@ -2,37 +2,43 @@ import unittest
 import re
 
 
-from regex_test import findCondorUserIDs, findEmail, cleanCondor, cleanGlidein
+from regex_test import cleanCondor, cleanGlidein, findCondorUserIDs, findEmail, findGlideinUserIDs
 IP_REGEX = ["(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)", "(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|^::(?:[0-9a-fA-F]{1,4}:){0,6}[0-9a-fA-F]{1,4}$|^[0-9a-fA-F]{1,4}::(?:[0-9a-fA-F]{1,4}:){0,5}[0-9a-fA-F]{1,4}$|^[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}::(?:[0-9a-fA-F]{1,4}:){0,4}[0-9a-fA-F]{1,4}$|^(?:[0-9a-fA-F]{1,4}:){0,2}[0-9a-fA-F]{1,4}::(?:[0-9a-fA-F]{1,4}:){0,3}[0-9a-fA-F]{1,4}$|^(?:[0-9a-fA-F]{1,4}:){0,3}[0-9a-fA-F]{1,4}::(?:[0-9a-fA-F]{1,4}:){0,2}[0-9a-fA-F]{1,4}$|^(?:[0-9a-fA-F]{1,4}:){0,4}[0-9a-fA-F]{1,4}::(?:[0-9a-fA-F]{1,4}:)?[0-9a-fA-F]{1,4}$|^(?:[0-9a-fA-F]{1,4}:){0,5}[0-9a-fA-F]{1,4}::[0-9a-fA-F]{1,4}$|(?:[0-9a-fA-F]{1,4}:){0,6}[0-9a-fA-F]{1,4}::"]
 
 class TestFilter(unittest.TestCase):
-    #def test_filter_init(self):
+    # def test_filter_init(self):
 
     def test_glidein(self): #Glidein Logs
         file1 = "regular_logs/overall_test_in.txt"
+        user_ids2 = findGlideinUserIDs(file1)
         cleanGlidein(file1)
-        line = (open(file1)).read()
+        file = open(file1, "r", encoding="utf8")
+        line = file.read()
+        file.close()
+        
         self.assertNotRegex(line, IP_REGEX[0])
         self.assertNotRegex(line, IP_REGEX[1])
+        self.assertNotRegex(line, rf"{user_ids2[0]}[\w \W]{user_ids2[1]}")
+        for x in range(0, len(user_ids2)):
+            self.assertFalse(line.find(user_ids2[x]) != -1)
+ 
         
-        #need to check that the glidein user ids are gone
 
-
-    """ def test_condor(self):
-        file2 = "condor_logs/job.1.StartdLog.txt"
-        
+    def test_condor(self):
+        file2 = "condor_logs/ip.txt"
         user_ids2 = findCondorUserIDs(file2)
         email = findEmail(file2)
         cleanCondor(file2)
+        file = open(file2)
+        line = file.read()
+        file.close()
         
-        f_in = (open(file2)).read()
-        
-        self.assertFalse(f_in.find(email) != -1) """
-        
-    """  for x in range(0, len(user_ids2)):
-            self.assertFalse(f_in.find(user_ids2[x])) """
-
-   
+        self.assertFalse(line.find(email) != -1)
+        self.assertNotRegex(line, IP_REGEX[0])
+        self.assertNotRegex(line, IP_REGEX[1])
+        for x in range(0, len(user_ids2)):
+            self.assertFalse(line.find(user_ids2[x]) != -1)
+ 
 
 if __name__ == '__main__':
     unittest.main()
